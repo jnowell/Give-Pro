e350083902c6c91ae027a63ec6ed351dbe15a674
Justin Nowell <jrnowell@gmail.com>
2017-03-27 00:34:20 -0400

    renaming password column

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20170325154912) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "income"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
