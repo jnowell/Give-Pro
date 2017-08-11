d3a69e9a592ae8f7f2ba449eeb60f62f3fb7f3e9
Justin Nowell <jrnowell@gmail.com>
2017-08-10 22:56:39 -0400

    Updating import flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,53 +80,65 @@ class UsersController < ApplicationController
     service.client_options.application_name = "GivePro" 
     service.authorization = auth_client
 
-    processors = ::Processor.all
+    logged_in_user = User.find(session[:user_id])
 
-    processor_string = ""
-    for processor in processors
-      unless (processor.domain_name.end_with? ".org" or processor.check_donation_string.present?)
-        processor_string << "|"
-        processor_string << processor.domain_name
-      end
-    end
-
-    puts "PROCESSOR STRING OF #{processor_string}"
-
-    # Show the user's labels
-    user_id = 'me'
-   
-    donation_count = 0
-    next_page_token = nil
-    label_id = 'CATEGORY_UPDATES'
-    #label_id = 'INBOX'
-    search_string = "subject:(thank|thanks|receipt|contribution|donation) from:(*.org#{processor_string})"
-    #search_string = "subject:(thank|thanks|receipt|contribution|donation) from:pih.org"
-    result = service.list_user_messages(user_id, label_ids: [label_id], q: search_string)
-    donation_count = parse_result(service,user_id, result)
+    puts "Logged In User of #{logged_in_user}"
     
-    puts "Result of #{result}"
+    
     logger.info "Calling new thread"
     Thread.new do 
-      while (result.next_page_token.present?)
-        result = service.list_user_messages(user_id, page_token: next_page_token, label_ids: [label_id], q: search_string)
+      ActiveRecord::Base.connection_pool.with_connection do
+        processors = ::Processor.all
+
+        processor_string = ""
+        for processor in processors
+          unless (processor.domain_name.end_with? ".org" or processor.check_donation_string.present?)
+            processor_string << "|"
+            processor_string << processor.domain_name
+          end
+        end
+
+        puts "PROCESSOR STRING OF #{processor_string}"
+
+        # Show the user's labels
+        user_id = 'me'
+       
+        donation_count = 0
+        next_page_token = nil
+        label_id = 'CATEGORY_UPDATES'
+        #label_id = 'INBOX'
+        #search_string = "subject:(thank|thanks|receipt|contribution|donation) from:(*.org#{processor_string})"
+        search_string = "subject:(thank|thanks|receipt|contribution|donation) from:actblue.com"
+
+        result = service.list_user_messages(user_id, label_ids: [label_id], q: search_string)
+        logger.info "Result of #{result}"
+        donation_count = parse_result(service,user_id, result, logged_in_user.email)
+        while (result.next_page_token.present?)
+          result = service.list_user_messages(user_id, page_token: next_page_token, label_ids: [label_id], q: search_string)
+          
+          donation_count += parse_result(service,user_id, result, logged_in_user.email)
+          puts "Donation count of #{donation_count}"
+          puts "Next page of #{result.next_page_token}"
+        end
+        processors_check_donation = Processor.where("check_donation_string is not null")
+        puts "PROCESSOR CHECK DONATION OF #{processors_check_donation}"
+        for processor in processors_check_donation
+          search_string = processor.check_donation_string + "from:" + processor.domain_name
+          result = service.list_user_messages(user_id, label_ids: [label_id], q: search_string)
+          puts "Result of #{result}"
+          donation_count += parse_result(service,user_id, result, logged_in_user.email)
+          puts "Donation count of #{donation_count}"
+        end
         
-        donation_count += parse_result(service,user_id, result)
-        puts "Donation count of #{donation_count}"
-        puts "Next page of #{result.next_page_token}"
+        puts "USER EMAIL OF #{logged_in_user.email}"
+        #if not this, try logged_in_user
+        send_donation_import_email(donation_count,logged_in_user.email)
+
+        logger.info "Ending thread"
+        #ActiveRecord::Base.connection.close  
+        #ActiveRecord::Base.connection_pool.releaseconnectiond
+        logger.info "Connection is closed"
       end
-      processors_check_donation = Processor.where("check_donation_string is not null")
-      puts "PROCESSOR CHECK DONATION OF #{processors_check_donation}"
-      for processor in processors_check_donation
-        search_string = processor.check_donation_string + "from:" + processor.domain
-        #result = service.list_user_messages(user_id, label_ids: [label_id], q: search_string)
-        puts "Result of #{result}"
-        #donation_count += parse_result(service,user_id, result)
-        puts "Donation count of #{donation_count}"
-        puts "Next page of #{result.next_page_token}"
-      end
-      logger.info "Ending thread"
-      ActiveRecord::Base.connection.close  
-      logger.info "Connection is closed"
     end
 
     #watch_request = Google::Apis::GmailV1::WatchRequest.new
@@ -136,7 +148,7 @@ class UsersController < ApplicationController
     #response = service.watch_user(user_id, watch_request)
     #puts response.inspect
 
-    if donation_count > 0
+    if false
       notice = 'We have scanned your email and noticed you made a few donations, so we imported them into our database.'
       notice << 'We are continuing to search for donations, and will add any that we find. If there are any donations that have not been added, please forward the receipts to receipts@givepro.io.'
       redirect_to donations_path, notice: notice
@@ -211,7 +223,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def parse_result(service, user_id, result)
+  def parse_result(service, user_id, result, user_email)
     count = 0
     donation_count = 0
     puts "Result messages of #{result.messages}"
@@ -263,16 +275,39 @@ class UsersController < ApplicationController
         #puts "Content of #{content}"
         logger.info "Date of #{date}"
         if content.present?
-          logged_in_user = User.find(session[:user_id])
-          logger.info "CALLING PARSE EMAIL WITH SUBJECT #{subject} AND FROM EMAIL #{from_email} AND TO EMAIL #{logged_in_user.email} AND DATE #{date}"
+          
+          logger.info "CALLING PARSE EMAIL WITH SUBJECT #{subject} AND FROM EMAIL #{from_email} AND TO EMAIL #{user_email} AND DATE #{date}"
           #scraper = .new
-          if MailScraper.parse_email(content, subject, from_email, logged_in_user.email, date)
+          if MailScraper.parse_email(content, subject, from_email, user_email, date)
             donation_count += 1
           end
         end
       end
     end
     return donation_count
+  end
+
+  def send_donation_import_email(donation_count,user_email)
+    #puts "AWS KEY id of "+.to_s
+    #intro_text = "Our script was unable to determine the donation amount in the following email. Please find a suitable regex and then edit the Regex field in <a href=\"http://www.givepro.io/non_profits/#{org.id}/edit\">this form</a> and click 'Submit'."
+    intro_text = "We were able to import #{donation_count} donations into our system. Please check your <a href='http://www.givepro.io/donations'>Donation Gallery</a> to see which donations we found. If there are any receipts you have that we might have missed, please forward them to receipts@givepro.io" 
+
+
+
+    ses = AWS::SES::Base.new(
+        :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+        :secret_access_key => ENV["AWS_SECRET_KEY"])
+
+    #print ses.buckets
+    
+    
+    ses.send_email(
+              :to        => user_email,
+              :source    => 'postmaster@givepro.io',
+              :subject   => 'Email Import Complete',
+              :html_body => intro_text
+    )
+    
   end
 
   private
